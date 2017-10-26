07087a3bbaf083d8f31c43bf6b94cea1a16b0bb8
Pragyan Subedi <pragyansubedi@gmail.com>
2017-10-26 17:42:59 +0545

    changed Downvote and Upvote functionality

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -64,14 +64,14 @@ class LinksController < ApplicationController
   def upvote
     @link =Link.find(params[:id])
     @link.upvote_by current_user
-    redirect_back fallback_location: root_path
+    
 
   end
 
   def downvote
     @link =Link.find(params[:id])
     @link.downvote_by current_user
-    redirect_back fallback_location: root_path
+    
   end
 
 
